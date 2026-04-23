ca7fb09f6fcd0badf2bffc1c0370fe1b6929dc1b
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2026-04-23 08:03:55 +0700

    vwap_engine

---
+++ cpp_engine/vwap_engine.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <numeric>
+#include <chrono>
+struct chunkResult{
+    double sumpv = 0;
+    double sumv = 0;
+};
+class VWAPEngine {
+public:
+};
+int main(){
+    return 0;
+}
