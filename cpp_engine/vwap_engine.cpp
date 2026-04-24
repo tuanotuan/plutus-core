d21a675927a13b6e9be3b3b42664e48f32b59d82
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2026-04-24 10:30:20 +0700

    calculateChunk

--- cpp_engine/vwap_engine.cpp
+++ cpp_engine/vwap_engine.cpp
@@ -8,7 +8,17 @@ struct chunkResult{
     double sumv = 0;
 };
 class VWAPEngine {
-public:
+    public:
+    static void calculateChunk(const std::vector<double>& prices, const std::vector<double>& volumes, int start, int end, chunkResult& result) {
+        double sumpv = 0;
+        double sumv = 0;
+        for (int i = start; i < end; ++i) {
+            sumpv += prices[i] * volumes[i];
+            sumv += volumes[i];
+        }
+        result.sumpv = sumpv;
+        result.sumv = sumv;
+    }
 };
 int main(){
     return 0;
