eaef1f7c757572b51c67fd93948c043d3ac2e3c7
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2026-04-25 14:29:32 +0700

    start computeVwapMultithread

--- cpp_engine/vwap_engine.cpp
+++ cpp_engine/vwap_engine.cpp
@@ -19,6 +19,17 @@ class VWAPEngine {
         result.sumpv = sumpv;
         result.sumv = sumv;
     }
+    static double computeVwapMultithread(const std::vector<double>& prices, const std::vector<double>& vols, int num_threads){
+        int total_ticks = prices.size();
+        if(total_ticks == 0) return 0.0;
+        std::vector < std::thread > threads;
+        std::vector < chunkResult > results(num_threads);
+        int chunk_size = total_ticks / num_threads;
+        for(int i = 0; i < num_threads; i++){
+            int start = i * chunk_size;
+            int end = (i == num_threads - 1) ? total_ticks : (i + 1) * chunk_size;
+        }
+    }
 };
 int main(){
     return 0;
