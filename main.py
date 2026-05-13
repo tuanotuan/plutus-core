fbd101d772f844492f0aff41aba8f6018d8d81a0
tuanotuan <unlbynnt@gmail.com>
2026-05-13 07:23:09 +0700

    update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # noi lap rap moi thu
 # main.py
 import time
+import json
 import pandas as pd
 from config.config import backTestConfig
 from data_loader import DataLoader
@@ -16,7 +17,15 @@ if __name__ == "__main__":
     try:
         prices, volumes, timestamps = loader.load_and_clean()
         
-        backtester = BacktestEngine(engine_bridge, prices, volumes, timestamps, config)
+        # 70% in-sample
+        with open("parameter/optimization_parameter.json", "r") as f:
+            params = json.load(f)
+        IS_RATIO = params["in_sample_ratio"]
+        split_idx = int(len(prices) * IS_RATIO)
+        prices_IS = prices[:split_idx]
+        volumes_IS = volumes[:split_idx]
+        timestamps_IS = timestamps[:split_idx]
+        backtester = BacktestEngine(engine_bridge, prices_IS, volumes_IS, timestamps_IS, config)
         
         print("\n[START BACKTESTING]")
         start_time = time.time()
@@ -28,7 +37,7 @@ if __name__ == "__main__":
         if not trade_history.empty:
             print(trade_history.to_string(index=False))
 
-            evaluator = Evaluator(trade_history, prices, timestamps, config.INITIAL_CAPITAL)
+            evaluator = Evaluator(trade_history, prices_IS, timestamps_IS, config.INITIAL_CAPITAL)
             evaluator.generate_report()
         else:
             print("Don't have any trades.")
